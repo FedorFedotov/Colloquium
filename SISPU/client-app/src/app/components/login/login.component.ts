b0c7ac7d0c7646e0458c836601e567c51dad029a
fedor.fedotov <fedotov@gapps.ispu.ru>
2018-12-16 21:31:18 +0300

    login/register/recovery GUI completed

--- SISPU/client-app/src/app/components/login/login.component.ts
+++ SISPU/client-app/src/app/components/login/login.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  login: boolean = true;
+  register: boolean = false;
+  forget: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  Start() {
+    this.login = true;
+    this.register = false;
+    this.forget = false;
+  }
+
+  
+  Register() {
+    this.login = false;
+    this.register = true;
+    this.forget = false;
+  }
+
+  Forget() {
+    this.login = false;
+    this.forget = true;
+    this.register = false;
   }
 
 }
